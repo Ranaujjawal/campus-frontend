b3dba1c0df221902a2eaa89d30f7f15eb208f694
Ranaujjawal <105837804+Ranaujjawal@users.noreply.github.com>
2023-06-13 12:56:52 +0530

    Update App.js line 43 revert the changes back

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         isRefreshNeeded={isDataBoxRefreshNeeded}
         onRefreshComplete={handleDataBoxRefreshComplete}
       />
-      <MSG onMessageSubmit={handleDataBoxRefresh} />
+      <MSG  handleRefresh={handleRefresh} onMessageSubmit={handleDataBoxRefresh} />
       <p className='alert'>The server gets reset at 1 A.M. to clear the database</p>
       <p className='alert'>If message does't recieve please refresh the page as it is free deployment speed is slow</p>
     </div>
